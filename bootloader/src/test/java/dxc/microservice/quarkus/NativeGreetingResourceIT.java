dbe97f3a6fd26b0234dd10d708718df59e17bf39
Alfredo Fernandez Manzano <afernandezma@dxc.com>
2022-11-14 13:27:16 +0100

    Update project structure to match hexagonal multi-module project

--- bootloader/src/test/java/dxc/microservice/quarkus/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package dxc.microservice.quarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends PropResourceTest {
-
-    // Execute the same tests but in native mode.
-}
