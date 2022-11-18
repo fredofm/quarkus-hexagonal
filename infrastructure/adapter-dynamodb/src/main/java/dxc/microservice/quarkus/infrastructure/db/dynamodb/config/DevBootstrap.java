272c662babd22523dd8a91c21519fa4147103ee3
Alfredo Fernandez Manzano <afernandezma@dxc.com>
2022-11-18 13:34:32 +0100

    Fix issue with hot reload using Lombok and avoid populating dynamodb table if it already exists

--- infrastructure/adapter-dynamodb/src/main/java/dxc/microservice/quarkus/infrastructure/db/dynamodb/config/DevBootstrap.java
+++ infrastructure/adapter-dynamodb/src/main/java/dxc/microservice/quarkus/infrastructure/db/dynamodb/config/DevBootstrap.java
@@ -10,7 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.DescribeTableEnhancedResponse;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.ResourceNotFoundException;
+import software.amazon.awssdk.services.dynamodb.model.TableDescription;
 
 @ApplicationScoped
 @Slf4j
@@ -20,12 +23,14 @@ public class DevBootstrap {
     @Inject
     DynamoDbClient dynamoDbClient;
 
-    void onStart(@Observes StartupEvent ev) {        
+    void onStart(@Observes StartupEvent ev) {
         log.info("Bootstrapping dynamodb database");
 
-        DynamoDbTable<PropEntity> propTable = createTable(PROPS_TABLE);
+        if (!tableExists(PROPS_TABLE)) {
+            DynamoDbTable<PropEntity> propTable = createTable(PROPS_TABLE);
 
-        populateTable(propTable);
+            populateTable(propTable);
+        }
     }
 
     private void populateTable(DynamoDbTable<PropEntity> propTable) {
@@ -38,7 +43,8 @@ public class DevBootstrap {
     private DynamoDbTable<PropEntity> createTable(String tableName) {
         log.info("Creating dynamodb table {}", tableName);
 
-        DynamoDbEnhancedClient dynamoDbEnhancedClient = DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient).build();
+        DynamoDbEnhancedClient dynamoDbEnhancedClient = DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient)
+                .build();
 
         TableSchema<PropEntity> tableSchema = TableSchema.fromClass(PropEntity.class);
         DynamoDbTable<PropEntity> propTable = dynamoDbEnhancedClient.table(PROPS_TABLE, tableSchema);
@@ -47,5 +53,24 @@ public class DevBootstrap {
         dynamoDbClient.waiter().waitUntilTableExists(b -> b.tableName(PROPS_TABLE));
 
         return propTable;
+    }
+
+    private boolean tableExists(String tableName) {
+        boolean tableExists = false;
+        DynamoDbEnhancedClient dynamoDbEnhancedClient = DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient)
+                .build();
+        TableSchema<PropEntity> tableSchema = TableSchema.fromClass(PropEntity.class);
+
+        try {
+            DescribeTableEnhancedResponse tableDescription = dynamoDbEnhancedClient.table(tableName, tableSchema)
+                    .describeTable();
+            log.info("Table {} already exists: {}", tableName, tableDescription);
+
+            tableExists = true;
+        } catch (ResourceNotFoundException rnfe) {
+            log.info("Table {} does not exist", tableName);
+        }
+
+        return tableExists;
     }
 }
--- infrastructure/adapter-dynamodb/src/main/java/dxc/microservice/quarkus/infrastructure/db/dynamodb/config/DynamoDBConfig.java
+++ infrastructure/adapter-dynamodb/src/main/java/dxc/microservice/quarkus/infrastructure/db/dynamodb/config/DynamoDBConfig.java
@@ -2,21 +2,14 @@ package dxc.microservice.quarkus.infrastructure.db.dynamodb.config;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 
 import dxc.microservice.quarkus.infrastructure.db.dynamodb.dbo.PropEntity;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @ApplicationScoped
 public class DynamoDBConfig {
-
-   /*  @Produces
-    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient basicClient) {
-        return DynamoDbEnhancedClient.builder().dynamoDbClient(basicClient).build();        
-    } */
 
     @Produces
     public DynamoDbTable<PropEntity> propTable(DynamoDbEnhancedClient enhancedClient) {
